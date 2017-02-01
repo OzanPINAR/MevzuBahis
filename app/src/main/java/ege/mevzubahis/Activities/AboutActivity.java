af0d1990a0e03e2c31371f30fd911e037bfc9121
ege <egek92@yandex.com>
2017-02-01 11:49:58 +0300

    about page added

---
+++ app/src/main/java/ege/mevzubahis/Activities/AboutActivity.java
@@ -0,0 +1,43 @@
+package ege.mevzubahis.Activities;
+
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
+import java.util.Calendar;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
+
+public class AboutActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+
+    Element adsElement = new Element();
+    adsElement.setTitle("Bet with friends, not with machines!");
+
+    View aboutPage = new AboutPage(this)
+        .isRTL(false)
+        .addItem(new Element().setTitle("Version 1.0"))
+        .addItem(adsElement)
+        .addGroup("Contact")
+
+        .addFacebook("mevzubahisofficial")
+        .addTwitter("mevzubahisOFC")
+
+
+
+        .addGitHub("egek92")
+
+        .create();
+
+    setContentView(aboutPage);
+  }
+
+
+
+}
