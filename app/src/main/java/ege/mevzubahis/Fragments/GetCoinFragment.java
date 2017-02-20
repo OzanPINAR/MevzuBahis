55e04efb4e7821c6a14a0a384f20397f39803a71
ege <egek92@yandex.com>
2017-02-20 22:25:36 +0300

    rewarded ad part 1

--- app/src/main/java/ege/mevzubahis/Fragments/GetCoinFragment.java
+++ app/src/main/java/ege/mevzubahis/Fragments/GetCoinFragment.java
@@ -1,19 +1,21 @@
 package ege.mevzubahis.Fragments;
 
-import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import ege.mevzubahis.R;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link GetCoinFragment.OnFragmentInteractionListener} interface
+ * {@link OnFragmentInteractionListener} interface
  * to handle interaction events.
  * Use the {@link GetCoinFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -23,6 +25,7 @@ public class GetCoinFragment extends Fragment {
   // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
   private static final String ARG_PARAM1 = "param1";
   private static final String ARG_PARAM2 = "param2";
+  @BindView(R.id.watch_ad_button) Button watchAdButton;
 
   // TODO: Rename and change types of parameters
   private String mParam1;
@@ -60,9 +63,12 @@ public class GetCoinFragment extends Fragment {
     }
   }
 
-  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_get_coin, container, false);
+    View view = inflater.inflate(R.layout.fragment_get_coin, container, false);
+    ButterKnife.bind(this, view);
+    return view;
   }
 
   // TODO: Rename method, update argument and hook method into UI event
@@ -86,16 +92,14 @@ public class GetCoinFragment extends Fragment {
     mListener = null;
   }
 
-  /**
-   * This interface must be implemented by activities that contain this
-   * fragment to allow an interaction in this fragment to be communicated
-   * to the activity and potentially other fragments contained in that
-   * activity.
-   * <p>
-   * See the Android Training lesson <a href=
-   * "http://developer.android.com/training/basics/fragments/communicating.html"
-   * >Communicating with Other Fragments</a> for more information.
-   */
+  @OnClick(R.id.watch_ad_button) public void onClick() {
+
+    //TODO: admob rewarded ad baglicam buraya
+
+
+  }
+
+
   public interface OnFragmentInteractionListener {
     // TODO: Update argument type and name
     void onFragmentInteraction(Uri uri);
