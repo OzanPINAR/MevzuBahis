532c6f6d91a44feaf3a272d33749053d7011f196
ege <egek92@kroot.org>
2017-04-26 18:03:40 +0300

    bets dialog

---
+++ app/src/main/java/ege/mevzubahis/Activities/FriendActivity.java
@@ -0,0 +1,13 @@
+package ege.mevzubahis.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ege.mevzubahis.R;
+
+public class FriendActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_friend);
+  }
+}
--- app/src/main/java/ege/mevzubahis/Fragments/BetsDialogFragment.java
+++ app/src/main/java/ege/mevzubahis/Fragments/BetsDialogFragment.java
@@ -1,64 +1,86 @@
 package ege.mevzubahis.Fragments;
 
-import android.support.v4.app.DialogFragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import ege.mevzubahis.Activities.FriendActivity;
 import ege.mevzubahis.R;
 
 /**
  * Created by Acer Bilgisayar on 18.4.2017.
  */
 
-public class BetsDialogFragment extends DialogFragment implements View.OnClickListener{
+public class BetsDialogFragment extends DialogFragment implements View.OnClickListener {
 
-    TextView matchName;
-    TextView duration;
-    RadioButton homeWins;
-    RadioButton draw;
-    RadioButton awayWins;
-    TextView coin;
-    EditText editText;
-    Button button;
-    TextView databaseDuration;
+  @BindView(R.id.tv_match) TextView tv_match;
+  @BindView(R.id.textView2) TextView textView2;
+  @BindView(R.id.textInputLayout) TextInputLayout textInputLayout;
+  @BindView(R.id.radioButton4) RadioButton radioButton4;
+  @BindView(R.id.radioButton5) RadioButton radioButton5;
+  @BindView(R.id.radioButton6) RadioButton radioButton6;
+  @BindView(R.id.button2) Button sendButton;
 
+  @Nullable @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.custom_dialog, null);
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.custom_dialog,null);
-        matchName=(TextView)view.findViewById(R.id.matchName);
-        duration=(TextView)view.findViewById(R.id.duration);
-        homeWins=(RadioButton)view.findViewById(R.id.homeWins);
-        draw=(RadioButton)view.findViewById(R.id.draw);
-        awayWins=(RadioButton)view.findViewById(R.id.awayWins);
-        coin=(TextView)view.findViewById(R.id.coin);
-        editText=(EditText)view.findViewById(R.id.editText);
-        button=(Button)view.findViewById(R.id.button2);
-        databaseDuration=(TextView)view.findViewById(R.id.databaseDuration);
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference reference = database.getReference("Bets").child("Sports");
 
-        button.setOnClickListener(this);
+    reference.addListenerForSingleValueEvent(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        for (DataSnapshot child : dataSnapshot.getChildren()){
+          String betsItem = child.getKey();
 
-        return view;
-    }
+          tv_match.setText(betsItem);
 
-    @Override
-    public void onClick(View v) {
-        if(v.getId()== R.id.button2){
-            dismiss();
-            Toast.makeText(getActivity(),"Button is clicked",Toast.LENGTH_LONG).show();
         }
+      }
 
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+
+      }
+    });
+
+    ButterKnife.bind(this, view);
+    return view;
+  }
+
+  @OnClick({ R.id.radioButton4, R.id.radioButton5, R.id.radioButton6, R.id.button2 })
+  public void onClick(View view) {
+    switch (view.getId()) {
+      case R.id.radioButton4:
+        break;
+      case R.id.radioButton5:
+        break;
+      case R.id.radioButton6:
+        break;
+      case R.id.button2:
+
+        //TODO: coin amount control
+        Intent intent = new Intent(getActivity(), FriendActivity.class);
+        startActivity(intent);
+
+        break;
     }
+  }
 }
