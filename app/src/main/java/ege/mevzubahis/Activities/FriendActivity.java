cbeb7890d16bade3231b236c948b8bc132624695
Yaşar Anıl Sansak <anilsansak@gmail.com>
2017-04-27 18:18:22 +0300

    friend list view (not properly functional)

--- app/src/main/java/ege/mevzubahis/Activities/FriendActivity.java
+++ app/src/main/java/ege/mevzubahis/Activities/FriendActivity.java
@@ -1,8 +1,12 @@
 package ege.mevzubahis.Activities;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,6 +23,7 @@ import org.json.JSONObject;
 import org.w3c.dom.Text;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import ege.mevzubahis.R;
 
@@ -28,6 +33,7 @@ public class FriendActivity extends AppCompatActivity {
   private TextView choiceText;
   private TextView coinText;
   private ListView friendList;
+
   String choice;
   String coin;
 
@@ -35,17 +41,36 @@ public class FriendActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_friend);
-    choiceText = (TextView) findViewById(R.id.choiceText);
+      final ListView friendList;
+      final ArrayList<String> friendList1 = new ArrayList<>();
+      final ArrayAdapter arrayAdapter;
+
     coinText = (TextView) findViewById(R.id.coinText);
     friendList = (ListView) findViewById(R.id.friendList);
 
-    Bundle b = getIntent().getExtras();
+      arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, friendList1){
+          @Override
+          public View getView(int position, View convertView, ViewGroup parent) {
+              View view =super.getView(position, convertView, parent);
+
+              TextView textView=(TextView) view.findViewById(android.R.id.text1);
+              textView.setTextColor(Color.BLACK);
+
+
+              return view;
+          }
+      };
+      friendList.setAdapter(arrayAdapter);
+
+
+
+      Bundle b = getIntent().getExtras();
 
     choice = b.getString("choice");
     coin = b.getString("coin");
 
-    coinText.setText(coin);
-    choiceText.setText(choice);
+    //coinText.setText(coin);
+    //choiceText.setText(choice);
       AccessToken accessToken = AccessToken.getCurrentAccessToken();
       GraphRequestBatch batch = new GraphRequestBatch(
               GraphRequest.newMyFriendsRequest(accessToken, new GraphRequest.GraphJSONArrayCallback() {
@@ -56,15 +81,20 @@ public class FriendActivity extends AppCompatActivity {
                               // Application code for users friends
                               Log.e("getsData completed" , jsonArray.toString());
                               Log.e("getData" , response.toString());
-                              try {
-                                  JSONObject jsonObject = response.getJSONObject();
-                                  Log.e("deneme3" , jsonObject.toString());
-                                  JSONObject summary = jsonObject.getJSONObject("summary");
-                                  Log.e("summary total" , summary.getString("total_count"));
-                              } catch (Exception e) {
-                                  e.printStackTrace();
+
+                                  try {
+                                      JSONObject jsonObject = response.getJSONObject();
+                                      Log.e("deneme3", jsonObject.toString());
+                                      JSONObject summary = jsonObject.getJSONObject("summary");
+                                      Log.e("summary total", summary.getString("total_count"));
+
+                                      friendList1.add(jsonArray.toString());
+                                      arrayAdapter.notifyDataSetChanged();
+                                  } catch (Exception e) {
+                                      e.printStackTrace();
+                                  }
                               }
-                          }
+
                       })
 
       );
